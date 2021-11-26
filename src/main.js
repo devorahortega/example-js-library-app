b7aa78593392dad5b6b8ecca8dd1fae59b5681e5
Devorah Ortega <devorahb.ortega@gmail.com>
2021-11-25 19:33:05 -0500

    Adds an attempt at a javascript library

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { Calendar } from "@fullcalendar/core";
+import listPlugin from "@fullcalendar/list";
 
 Vue.config.productionTip = false;
 
@@ -8,3 +10,8 @@ new Vue({
   router,
   render: (h) => h(App),
 }).$mount("#app");
+
+let calendar = new Calendar(calendarEl, {
+  plugins: [listPlugin],
+  initialView: "listWeek",
+});
